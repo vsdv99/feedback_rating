d4d2fb90d10d2109f100e07152538c582568894c
Vitor Valle <81134886+vsdv99@users.noreply.github.com>
2023-03-08 12:57:37 -0300

    backgroundcolor bug solved

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
 const list = document.querySelectorAll('.main__rating')
-const submitBtn  = document.querySelector('.main__btn')
-const mainSection = document.querySelector('.main')
-const thankYouSection = document.querySelector('.thank_you')
+const btn  = document.querySelector('.main__btn')
+
+const main = document.querySelector('.main')
+const thankYou = document.querySelector('.thank_you')
+
+let feedbackContent = 0
 let feedbackResult = document.querySelector('#feedback')
-let feedbackValue = 0
 
 
 
 list.forEach((el) => {
-    el.addEventListener('click',(e) => {
-        console.log(e.target.textContent)
-        feedbackValue = e.target.textContent
+    el.addEventListener('click',(ev) => {
+        feedbackContent = ev.target.textContent
         for(let child of el.children){
             child.classList.remove('active')
         }
-        e.target.classList.toggle('active')
+        ev.target.classList.toggle('active')
     })
     
 })
 
 
-submitBtn.addEventListener('click',() => {
-    if (feedbackValue != 0){
-        mainSection.classList.add('hidden')
-        thankYouSection.classList.remove('hidden')
-        feedbackResult.textContent = `You selected ${feedbackValue} out of 5`
+btn.addEventListener('click',() => {
+    if (feedbackContent != 0){
+        main.classList.add('hidden')
+        thankYou.classList.remove('hidden')
+        feedbackResult.textContent = `You selected ${feedbackContent} out of 5`
     }else{
         alert('Please provide Feedback first')
     }
